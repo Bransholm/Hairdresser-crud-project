613f1b05486f01aa21874fcdb1d417b4ac8a766b
Gorm-git <tneermark@gmail.com>
2023-04-28 20:25:25 +0200

    Yep

--- booking.js
+++ booking.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const endpoint =
+const endpoint1 =
   "https://hairdresser-crud-project-default-rtdb.europe-west1.firebasedatabase.app/";
 
 // I vores CRUD APP er der en global function som hedder "let posts;". Jeg er usikker på om vi også skal have en lignende global variabel
