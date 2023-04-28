d515a143155cc8f74283a23905581a23f0474938
Gorm-git <tneermark@gmail.com>
2023-04-28 15:00:29 +0200

    Const endpoint (firebase)

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 "use strict";
 
+const endpoint =
+  "https://hairdresser-crud-project-default-rtdb.europe-west1.firebasedatabase.app/";
+
+// I vores CRUD APP er der en global function som hedder "let posts;". Jeg er usikker på om vi også skal have en lignende global variabel
+// let posts;
+
 window.addEventListener("load", start);
 
 function start() {
