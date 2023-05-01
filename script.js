10e46bb552872f4850bc9acdd9315b843c21b8f8
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-05-01 14:57:46 +0200

    lort

--- script.js
+++ script.js
@@ -7,4 +7,50 @@ window.addEventListener("load", start);
 
 function start() {
   console.log("Hi hairdresser");
+  showFrontPage();
+
+  document
+    .querySelector("#frontpageLink")
+    .addEventListener("click", showFrontPage);
+  document
+    .querySelector("#bookingLink")
+    .addEventListener("click", showBookingPage);
 }
+
+function showFrontPage() {
+  // document.querySelector("#frontpage").classList.remove("hidden");
+  document.querySelector("#booking").classList.add("hidden");
+  document.querySelector("#shopInfo").classList.add("hidden");
+}
+
+// function showBookingPage() {
+//   document.querySelector("#booking").classList.remove("hidden");
+//   document.querySelector("#frontpage").classList.add("hidden");
+//   document.querySelector("#shop-info").classList.add("hidden");
+//   document.querySelector("#photos").classList.add("hidden");
+//   document.querySelector("#conacts").classList.add("hidden");
+// }
+
+// function showShopPage() {
+//   document.querySelector("#shop-info").classList.remove("hidden");
+//   document.querySelector("#booking").classList.add("hidden");
+//   document.querySelector("#frontpage").classList.add("hidden");
+//   document.querySelector("#photos").classList.add("hidden");
+//   document.querySelector("#conacts").classList.add("hidden");
+// }
+
+// function showPhotosPage() {
+//   document.querySelector("#photos").classList.remove("hidden");
+//   document.querySelector("#booking").classList.add("hidden");
+//   document.querySelector("#frontpage").classList.add("hidden");
+//   document.querySelector("#shop-info").classList.add("hidden");
+//   document.querySelector("#conacts").classList.add("hidden");
+// }
+
+// function showContactsPage() {
+//   document.querySelector("#conacts").classList.remove("hidden");
+//   document.querySelector("#booking").classList.add("hidden");
+//   document.querySelector("#frontpage").classList.add("hidden");
+//   document.querySelector("#shop-info").classList.add("hidden");
+//   document.querySelector("#photos").classList.add("hidden");
+// }
